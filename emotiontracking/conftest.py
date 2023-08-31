705c3ac98412d3af20f77460e5acdf2426cd9f9a
Abdellah <abdodo59@gmail.com>
2023-08-31 23:03:16 +0200

    up

--- emotiontracking/conftest.py
+++ emotiontracking/conftest.py
@@ -1,11 +1,30 @@
 import pytest
-from django.conf import settings
+from django.db import connections
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def run_sql(sql):
+    conn = psycopg2.connect(database='postgres')
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.close()
+
 
 @pytest.fixture(scope='session')
 def django_db_setup():
-    settings.DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgres',
-        'HOST': 'localhost',
-        'NAME': 'postgres',
-        'ATOMIC_REQUESTS': True
-}
+    from django.conf import settings
+
+    settings.DATABASES['default']['NAME'] = 'the_copied_db'
+
+    run_sql('DROP DATABASE IF EXISTS the_copied_db')
+    run_sql('CREATE DATABASE the_copied_db TEMPLATE the_source_db')
+
+    yield
+
+    for connection in connections.all():
+        connection.close()
+
+    run_sql('DROP DATABASE the_copied_db')
